4afa5c9c1e106ee389713ea1d7146d57ddee1c4f
Kasem Jaffer <kasem.jaffer@gmail.com>
2013-12-11 00:16:49 +0200

    Update DatabaseContext.java

--- AndroidSQLite/src/com/kas/androidsqlite/DatabaseContext.java
+++ AndroidSQLite/src/com/kas/androidsqlite/DatabaseContext.java
@@ -25,21 +25,25 @@ import com.google.gson.Gson;
  * Created by Kassim on 10/3/13.
  */
 public class DatabaseContext {
-	DBOpenHelper dbhelper;
-	SQLiteDatabase database;
+	
+	private DBOpenHelper dbhelper;
+	private SQLiteDatabase database;
+	private boolean isAutoOpenAndClose;
 
 /**
  * 
  * @param context
  * @param DB_NAME Database name in the assets folder for example data.db
+ * @param isAutoOpenAndClose if set to true, it will open the database before every operation and closes it after automatically
  * @throws IOException throws exception if the DB_NAME passed not found in the assets folder.
+ * @N.B BestPractice I recommend you to have one instance of this class through out your application. Hopefully, that way you won't have conflicts and everything would would work synchronously.
  */
-	public DatabaseContext(Context context, String DB_NAME) throws IOException {
+	public DatabaseContext(Context context, String DB_NAME, boolean isAutoOpenAndClose) throws IOException {
 		dbhelper = new DBOpenHelper(context, DB_NAME);
 		dbhelper.createDatabase();
+		this.isAutoOpenAndClose=isAutoOpenAndClose;
 	}
 
-	
 	/**
 	 * Opens the connection with the database
 	 */
@@ -77,6 +81,10 @@ public class DatabaseContext {
 	    */
 	
 	public <T> T add(T entity ) {
+		
+		if(isAutoOpenAndClose)
+			open();
+		
 		HashMap<String, List<Object>> externalObjects = new HashMap<String, List<Object>>();
 		
 		long insertid = database.insert(entity.getClass().getSimpleName(), null,getContentValues(entity, externalObjects));	
@@ -85,7 +93,39 @@ public class DatabaseContext {
 		for (Entry<String, List<Object>> entry : externalObjects.entrySet()) {
 			List<?extends Object> ext = addAll(entry.getValue(), entity.getClass().getSimpleName(),insertid);
 			setObjectField(entity, entry.getKey(), ext);
+		}	
+		
+		if(isAutoOpenAndClose)
+			close();
+		
+		return entity;
+	}
+	
+	/**
+	 * Adds an entity and if the entity has list of entities it will add them as well.
+	 * @param entity Entity to be added. The name of the class must match the name of the table in the database.
+	 * @param IdPropertyName pass the primary key name. This is in case the primary key is named other than "id" or "Id"
+	 * @param isAutoIncrement set true if the primary key is auto-increment
+	 * @return T returns the added Entity with id assigned .
+	 */
+	public <T> T add(T entity, String IdPropertyName, boolean isAutoIncrement) {
+		
+		if(isAutoOpenAndClose)
+			open();
+		
+		HashMap<String, List<Object>> externalObjects = new HashMap<String, List<Object>>();
+		
+		long insertid = database.insert(entity.getClass().getSimpleName(), null, getContentValues(entity, IdPropertyName,isAutoIncrement, externalObjects));	
+		
+		setObjectField(entity, IdPropertyName, insertid);
+		for (Entry<String, List<Object>> entry : externalObjects.entrySet()) {
+			List<?extends Object> ext = addAll(entry.getValue(), entity.getClass().getSimpleName(),insertid);
+			setObjectField(entity, entry.getKey(), ext);
 		}
+		
+		if(isAutoOpenAndClose)
+			close();
+		
 		return entity;
 	}
 	
@@ -100,6 +140,13 @@ public class DatabaseContext {
 		}	
 		return entities;		
 	}
+	
+	public <T> List<T> addAll (List<T> entities, String IdPropertyName, boolean isAutoIncrement){
+		for (T entity : entities) {
+			add(entity, IdPropertyName, isAutoIncrement);
+		}	
+		return entities;		
+	}
 		
 	/**
 	 * Gets all the records in a table in the database.
@@ -110,11 +157,20 @@ public class DatabaseContext {
 	 * getAll(new Employee(), true);
 	 */
 	public <T> List<T> getAll(T entity, boolean withAllItsListFields) {
+		
+		if(isAutoOpenAndClose)
+			open();
 			
 		Cursor cursor = database.query(entity.getClass().getSimpleName(), null, null, null, null, null, null);
 		
-		if(withAllItsListFields) return getDataWithAllItsReference(cursor, entity);
-		else return getDataFromCursor(cursor, entity);
+		List<T> entitiesToReturn;		
+		if(withAllItsListFields) entitiesToReturn= getDataWithAllItsReference(cursor, entity);
+		else entitiesToReturn = getDataFromCursor(cursor, entity);
+
+		if(isAutoOpenAndClose)
+			close();
+		
+		return entitiesToReturn;
 	}
 
 	/**
@@ -125,13 +181,21 @@ public class DatabaseContext {
 	 */
 	public <T> T find(T entity, boolean withAllItsListFields) {
 
+		if(isAutoOpenAndClose)
+			open();
 
-			Cursor cursor = database.query(entity.getClass().getSimpleName(), null,
+		Cursor cursor = database.query(entity.getClass().getSimpleName(), null,
 					"id=?", new String[] { String.valueOf(getIdFromObject(entity)) }, null, null, null,
 					null);
-				
-		if(withAllItsListFields) return getDataWithAllItsReference(cursor, entity).get(0);
-		else return getDataFromCursor(cursor, entity).get(0);			
+		
+		T entityToReturn;
+		if(withAllItsListFields) entityToReturn =  getDataWithAllItsReference(cursor, entity).get(0);
+		else entityToReturn = getDataFromCursor(cursor, entity).get(0);
+		
+		if(isAutoOpenAndClose)
+			close();
+		
+		return entityToReturn;
 	}
 
 	/**
@@ -140,6 +204,9 @@ public class DatabaseContext {
 	 * @return returns the number of rows affected.
 	 */
 	public int delete(Object entity){
+		
+		if(isAutoOpenAndClose)
+			open();
 		
 		HashMap<String, Object> iCollectionObject = new HashMap<String, Object>();		
 		getObjectFields(entity, iCollectionObject).toArray(new String[] {});
@@ -152,6 +219,37 @@ public class DatabaseContext {
 		int result = database.delete(entity.getClass().getSimpleName(), "id= ?",
                 new String[] { String.valueOf(getIdFromObject(entity)) });	
 		
+		if(isAutoOpenAndClose)
+			close();
+		
+		return result;
+	}
+	
+	/**
+	 * Deletes a record from a table in the database.
+	 * @param entity pass an entity to be deleted. the entity must have a property named id and is assigned. The name of its class must match the name of the table in the database.
+	 * @param IdPropertyName pass the primary key name. This is in case the primary key is named other than "id" or "Id"
+	 * @return returns the number of rows affected.
+	 */
+	public int delete(Object entity, String IdPropertyName){
+		
+		if(isAutoOpenAndClose)
+			open();
+		
+		HashMap<String, Object> iCollectionObject = new HashMap<String, Object>();		
+		getObjectFields(entity, iCollectionObject).toArray(new String[] {});
+		
+		for (Entry<String, Object> entry : iCollectionObject.entrySet()) {
+			database.delete(entry.getValue().getClass().getSimpleName(), entity.getClass().getSimpleName() +"= ?",
+	                new String[] { String.valueOf(getIdFromObject(entity))});	
+		}
+		
+		int result = database.delete(entity.getClass().getSimpleName(), IdPropertyName+"= ?",
+                new String[] { String.valueOf(getIdFromObject(entity, IdPropertyName)) });	
+		
+		if(isAutoOpenAndClose)
+			close();
+		
 		return result;
 	}
 
@@ -162,6 +260,10 @@ public class DatabaseContext {
 	 * @return returns the updated entity
 	 */
 	public <T> T update(T entity, boolean withAllItsListFields){
+		
+		if(isAutoOpenAndClose)
+			open();
+		
 		HashMap<String, List<Object>> externalObjects = new HashMap<String, List<Object>>();
 		ContentValues values = getContentValues(entity, externalObjects);
 		int result = database.update(entity.getClass().getSimpleName(), values, "id = ?",
@@ -173,18 +275,26 @@ public class DatabaseContext {
 				setObjectField(entity, entry.getKey(), ext);
 			}
 		}
+		
+		if(isAutoOpenAndClose)
+			close();
+		
 		if(result == 1) return entity;
-		else return setObjectField(entity, "id", 0);
+		else return setObjectField(entity, "id", 0);	
 	}
 	
 	/**
 	 * Updates a record in a table in the database
-	 * @param IdPropertyName in case the primary key is named other than "Id"
+	 * @param IdPropertyName in case the primary key is name other than Id
 	 * @param entity entity to be updated. the entity must have a property named id and is assigned. The name of its class must match the name of the table in the database.
 	 * @param withAllItsListFields if true, it will update the list properties in the entity. If false, the list properties will be not be updated.
 	 * @return returns the updated entity
 	 */
 	public <T> T update(T entity, String IdPropertyName,  boolean withAllItsListFields){
+		
+		if(isAutoOpenAndClose)
+			open();
+		
 		HashMap<String, List<Object>> externalObjects = new HashMap<String, List<Object>>();
 		ContentValues values = getContentValues(entity, externalObjects);
 		int result = database.update(entity.getClass().getSimpleName(), values, IdPropertyName+" = ?",
@@ -196,8 +306,28 @@ public class DatabaseContext {
 				setObjectField(entity, entry.getKey(), ext);
 			}
 		}
+		
+		if(isAutoOpenAndClose)
+			close();
+		
 		if(result == 1) return entity;
 		else return setObjectField(entity, IdPropertyName, 0);
+	}
+	
+	/**
+	 * Updates list of records in a table in the database
+	 * @param IdPropertyName in case the primary key is name other than Id
+	 * @param entities list of entities to be updated. All the entities must have a property named id and is assigned. The name of its class must match the name of the table in the database.
+	 * @param withAllItsInnerListFields if true, it will update the inner list properties in the entity. If false, the inner list properties will be not be updated.
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public <T> List<T> updateAll(List<T> entities, String IdPropertyName, boolean withAllItsInnerListFields){
+		List<Object> insertedObjects = new ArrayList<Object>();		
+		for(Object obj : (List<Object>) entities){
+			insertedObjects.add(update(obj,IdPropertyName, withAllItsInnerListFields));
+		}
+		return (List<T>) insertedObjects;
 	}
 	
 	/**
@@ -212,7 +342,6 @@ public class DatabaseContext {
 		for(Object obj : (List<Object>) entities){
 			insertedObjects.add(update(obj, withAllItsInnerListFields));
 		}
-		
 		return (List<T>) insertedObjects;
 	}
 
@@ -225,15 +354,27 @@ public class DatabaseContext {
 	 * @ForExample findAll(new Employee(), contentValues, true);
 	 * 
 	 */
-	public <T> List<T> findAll(T entity, ContentValues contentValues, boolean withAllItsListFields){
-
-		Cursor cursor = database.query(entity.getClass().getSimpleName(), null,
-				contentValues.toString(), null, null, null, null, null);
-		
-		if(withAllItsListFields) return getDataWithAllItsReference(cursor, entity);
-		else return getDataFromCursor(cursor, entity);
-	}
 	
+	public <T> List<T> findAll(T entity, ContentValues contentValues, boolean withAllItsListFields){
+		
+		if(isAutoOpenAndClose)
+			open();
+		
+		String tableName = entity.getClass().getSimpleName();
+		String where = getContentValuesAsStringSeperatedBy("AND", contentValues);
+		String sql = "SELECT * FROM "+tableName+" WHERE "+where;
+		Cursor cursor = database.rawQuery(sql, null);
+		
+		List<T> entitiesToReturn;
+		if(withAllItsListFields) entitiesToReturn = getDataWithAllItsReference(cursor, entity);
+		else entitiesToReturn = getDataFromCursor(cursor, entity);
+		
+		if(isAutoOpenAndClose)
+			close();
+		
+		return entitiesToReturn;
+	}
+
 	/**
 	 * Finds all matched records from a many to many relationship table
 	 * @param entityToReturn only pass instance of a class, for example new Employee(). Do not pass the class itself. The name of its class must match the name of the table in the database.
@@ -263,6 +404,9 @@ public class DatabaseContext {
 	 */
 	public <T> List<T> subQuery(T entityToReturn, Object fromEntity, Object whereEntity,  boolean withAllItsListFields){
 		
+		if(isAutoOpenAndClose)
+			open();
+		
 		Object equalval = getIdFromObject(whereEntity);
 		String whereKey = whereEntity.getClass().getSimpleName();
 		
@@ -275,19 +419,41 @@ public class DatabaseContext {
 
 		Cursor cursor = database.rawQuery(sql, null);
 	
-		if(withAllItsListFields) return getDataWithAllItsReference(cursor, entityToReturn);
-		else return getDataFromCursor(cursor, entityToReturn);
+		List<T> entitiesToReturn;
+		if(withAllItsListFields) entitiesToReturn = getDataWithAllItsReference(cursor, entityToReturn);
+		else entitiesToReturn = getDataFromCursor(cursor, entityToReturn);
+		
+		if(isAutoOpenAndClose)
+			close();
+		
+		return entitiesToReturn;
 	}
 	
 	/**
-	 * 
-	 * @param entityToReturn 
-	 * @param SQLQuery i.e select * from Employee where Employee.Name='Kasem'
-	 * @return returns list of entities passed
+	 * Runs the provided SQL and returns a user friendly List over the result set.
+	 * @param entityToReturn instance of the type Object to return in a List 
+	 * @param SQLQuery the SQL query. The SQL string must not be ; terminated
+	 * @return return List over the result set (List of the provided entityToReturn) 
+	 * @ForExample <pre>
+	 * <code>
+	 *	String sql = "SELECT * FROM Employee";
+	 *	List[Employee] employees = databaseContext.rawQuery(new Employee(),sql);
+	 * </code>
+	 * </pre>
 	 */
-	public <T> List<T> rawQuerry(T entityToReturn, String SQLQuery){	
+	public <T> List<T> rawQuery(T entityToReturn, String SQLQuery){
+		
+		if(isAutoOpenAndClose)
+			open();
+		
+		List<T> entitiesToReturn;
 		Cursor cursor = database.rawQuery(SQLQuery, null);
-		return getDataFromCursor(cursor, entityToReturn);
+		entitiesToReturn = getDataFromCursor(cursor, entityToReturn);
+		
+		if(isAutoOpenAndClose)
+			close();
+		
+		return entitiesToReturn;
 	}
 	
 	
@@ -336,6 +502,8 @@ public class DatabaseContext {
 		} else if (cursorWindow.isString(pos, index)) {
 			obj = cursor.getString(index);
 		}
+		
+		
 	 		
 //This method is not supported in api level < 11
 //		
@@ -397,10 +565,37 @@ public class DatabaseContext {
 				String key = field.getName();
 				Object value = field.get(obj);
 				
-				if (!key.equalsIgnoreCase("id")&& value!=null && !(value instanceof List<?>)) {								
+				if (!key.equalsIgnoreCase("id") && value!=null && !(value instanceof List<?>)) {								
 					values.put(key, String.valueOf(value));
 				}else if(value instanceof List<?>){
 					externalObjects.put(key, (List<Object>) value);
+				}
+			} catch (IllegalAccessException e) {}
+		return values;
+	}
+	
+	@SuppressWarnings("unchecked")
+	private ContentValues getContentValues(Object obj,String IdProperty, boolean isAutoIncrement,  HashMap<String, List<Object>> externalObjects) {
+		ContentValues values = new ContentValues();
+		Field[] fields = obj.getClass().getDeclaredFields();
+		for (Field field : fields)
+			try {
+				field.setAccessible(true);
+				String key = field.getName();
+				Object value = field.get(obj);
+				
+				if(isAutoIncrement){
+					if (!key.equalsIgnoreCase(IdProperty) && value!=null && !(value instanceof List<?>)) {								
+						values.put(key, String.valueOf(value));
+					}else if(value instanceof List<?>){
+						externalObjects.put(key, (List<Object>) value);
+					}
+				}else{
+					if (value!=null && !(value instanceof List<?>)) {								
+						values.put(key, String.valueOf(value));
+					}else if(value instanceof List<?>){
+						externalObjects.put(key, (List<Object>) value);
+					}
 				}
 			} catch (IllegalAccessException e) {}
 		return values;
@@ -503,5 +698,11 @@ public class DatabaseContext {
 		cursor.close();		
 		return (List<T>) jArray;	
 	}
+	
+	private String getContentValuesAsStringSeperatedBy(String speratedBy, ContentValues contentValues) {	
+		String c = contentValues.valueSet().toString();	
+		return c.replace("[", "").replace("=", "='").replaceAll(",","' "+speratedBy).replace("]", "'");
+	}
+	
 
 }
